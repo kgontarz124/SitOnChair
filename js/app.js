d9b1d0d10267031690bc6202dc4f94372b35893e
kgontarz124 <katarzynagontarz124@gmail.com>
2017-09-15 10:43:23 +0200

    projekt SitOnChair

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 /*SUBMENU - ZADANIE 1*/
 var firstElementOfNav = document.querySelector(".main-nav ul").firstElementChild;
-console.log(firstElementOfNav);
 var submenu = document.querySelector(".main-nav-submenu");
-console.log(submenu);
 
 //pokazanie sie submenu
 firstElementOfNav.addEventListener("mouseenter", function (){
@@ -19,10 +17,10 @@ submenu.addEventListener("mouseleave", function (){
 
 
 /*BOXES - ZADANIE 2*/
-var boxWithPictures = document.querySelector(".pictures");console.log(boxWithPictures);
-var picturesContent = boxWithPictures.querySelectorAll(".picture-cnt");console.log(picturesContent);
-var pictures = boxWithPictures.querySelectorAll("img");console.log(pictures);
-var descriptions = boxWithPictures.querySelectorAll(".picture-cnt-description");console.log(descriptions);
+var boxWithPictures = document.querySelector(".pictures");
+var picturesContent = boxWithPictures.querySelectorAll(".picture-cnt");
+var pictures = boxWithPictures.querySelectorAll("img");
+var descriptions = boxWithPictures.querySelectorAll(".picture-cnt-description");
 
 //pierwszy obrazek
 pictures[0].addEventListener("mouseenter", function (){
@@ -43,11 +41,8 @@ pictures[1].addEventListener("mouseleave", function (){
 
 /*SLIDER - ZADANIE 3*/
 var nextButton = document.querySelector(".right");
-console.log(nextButton);
 var prevButton = document.querySelector(".left");
-console.log(prevButton);
 var sliderElements = document.querySelector(".slider").children;
-console.log(sliderElements);
 
 
 var counter = 0;
@@ -78,3 +73,103 @@ prevButton.addEventListener("click", function (){
             sliderElements[counter].classList.add("active");
         }
 });
+
+
+
+/*DODATKOWE*/
+var arrows = document.querySelectorAll(".form .drop_down_list .list_arrow");
+var panels = document.querySelectorAll(".form .drop_down_list .list_panel");
+var counter = 0;
+
+//pokazanie się i chowanie panelow po nacisnieciu strzalki
+for (var i=0; i<arrows.length; i++) {
+    arrows[i].addEventListener("click", function () {
+        counter++;
+        if (counter === 1) {
+            this.nextElementSibling.style.display = "inline-block";
+        } else {
+            this.nextElementSibling.style.display = "none";
+            counter = 0;
+        }
+    });
+}
+
+
+
+/*POLA WYBORU I PODSUMOWANIA*/
+//typ fotela
+var allLabels = document.querySelectorAll(".list_label");
+
+
+var sum = document.querySelector(".sum_label");
+var costs = document.querySelector(".panel_right").children;
+var totalSum = 0;
+
+var  typeOfChairLabel = allLabels[0];
+var choiseTypeOfChair = document.querySelector(".panel_left h4");
+var priceTypeOfChair = document.querySelector(".panel_right h4");
+var typeOfChair = panels[0].children;
+
+
+for (var i=0; i<typeOfChair.length; i++) {
+    typeOfChair[i].addEventListener("click", function () {
+        typeOfChairLabel.innerText = this.innerText;
+        choiseTypeOfChair.innerText = this.innerText;
+        priceTypeOfChair.innerText = this.dataset.price;
+        totalSum += Number(priceTypeOfChair.innerText);
+        sum.innerText = "SUMA : " + totalSum;
+    });
+}
+
+//kolor fotela
+var  colorOfChairLabel = allLabels[1];
+var choiseColorOfChair = document.querySelector(".panel_left .color");
+var priceColorOfChair = document.querySelector(".panel_right .color");
+var colorOfChair = panels[1].children;
+
+for (var i=0; i<typeOfChair.length; i++) {
+    colorOfChair[i].addEventListener("click", function () {
+        colorOfChairLabel.innerText = this.innerText;
+        choiseColorOfChair.innerText = this.innerText;
+        priceColorOfChair.innerText = "0";
+        totalSum += Number(priceColorOfChair.innerText);
+        sum.innerText = "SUMA : " + totalSum;
+    });
+}
+
+//rodzaj materialu
+var  patternOfChairLabel = allLabels[2];
+var choisePatternOfChair = document.querySelector(".panel_left .pattern");
+var pricePatternOfChair = document.querySelector(".panel_right .pattern");
+var patternOfChair = panels[2].children;
+
+for (var i=0; i<patternOfChair.length; i++) {
+    patternOfChair[i].addEventListener("click", function () {
+        patternOfChairLabel.innerText = this.innerText;
+        choisePatternOfChair.innerText = this.innerText;
+        pricePatternOfChair.innerText = this.dataset.price;
+        totalSum += Number(pricePatternOfChair.innerText);
+        sum.innerText = "SUMA : " + totalSum;
+    });
+}
+
+//wybor transportu
+var checkboxTransport = document.querySelector("#transport");
+var choiseTransport = document.querySelector(".panel_left .transport");
+var priceTransport = document.querySelector(".panel_right .transport");
+
+checkboxTransport.addEventListener("click", function () {
+    if (this.checked) {
+        choiseTransport.innerText = "Transport";
+        priceTransport.innerText = this.dataset.price;
+        totalSum += Number(priceTransport.innerText);
+        sum.innerText = "SUMA : " + totalSum;
+    } else {
+        choiseTransport.innerText = "";
+        priceTransport.innerText = "";
+    }
+});
+
+
+
+sum.innerText = "SUMA : " + totalSum;
